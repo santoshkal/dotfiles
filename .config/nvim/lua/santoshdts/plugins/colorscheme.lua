748a02e13c9f9efa7896a802fceee8bc1c1c018f
Santosh <ksantosh@intelops.dev>
2024-07-04 21:54:25 +0530

    Custom color on cyderdream

--- .config/nvim/lua/santoshdts/plugins/colorscheme.lua
+++ .config/nvim/lua/santoshdts/plugins/colorscheme.lua
@@ -1,27 +1,62 @@
 return {
 	{
 		"scottmckendry/cyberdream.nvim",
-		dev = true,
+		dev = false,
 		lazy = false,
 		priority = 1000,
 		config = function()
 			require("cyberdream").setup({
+				-- Enable transparent background
 				transparent = false,
+
+				-- Enable italics comments
 				italic_comments = true,
-				hide_fillchars = true,
-				terminal_colors = false,
-				borderless_telescope = { border = false, style = "flat" },
+
+				-- Replace all fillchars with ' ' for the ultimate clean look
+				hide_fillchars = false,
+
+				-- Modern borderless telescope theme
+				borderless_telescope = true,
+
+				-- Set terminal colors used in `:terminal`
+				terminal_colors = true,
+
 				theme = {
-					variant = "auto",
-					overrides = function(colours)
+					variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+					highlights = {
+						-- Highlight groups to override, adding new groups is also possible
+						-- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+						-- Example:
+						Comment = { fg = "#696969", bg = "NONE", italic = true },
+
+						-- Complete list can be found in `lua/cyberdream/theme.lua`
+					},
+
+					-- Override a highlight group entirely using the color palette
+					overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
+						-- Example:
 						return {
-							TelescopePromptPrefix = { fg = colours.blue },
-							TelescopeMatching = { fg = colours.cyan },
-							TelescopeResultsTitle = { fg = colours.blue },
-							TelescopePromptCounter = { fg = colours.cyan },
-							TelescopePromptTitle = { fg = colours.bg, bg = colours.blue, bold = true },
+							Comment = { fg = colors.lightgreen, bg = "NONE", italic = true },
+							["@property"] = { fg = colors.magenta, bold = true },
 						}
 					end,
+
+					-- Override a color entirely
+					colors = {
+						-- For a list of colors see `lua/cyberdream/colours.lua`
+						-- Example:
+						bg = "#042416",
+						green = "#034961",
+						magenta = "#ff00ff",
+					},
+				},
+
+				-- Disable or enable colorscheme extensions
+				extensions = {
+					telescope = true,
+					notify = true,
+					mini = true,
 				},
 			})
 
