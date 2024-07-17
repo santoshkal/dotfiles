19308618b852808f5044abe6a83bae938bba7b08
Santosh <ksantosh@intelops.dev>
2024-07-17 19:44:45 +0530

    Remove chatGPT

--- .config/nvim/lua/santoshdts/plugins/extras/chatgpt.lua
+++ .config/nvim/lua/santoshdts/plugins/extras/chatgpt.lua
@@ -13,6 +13,15 @@ return {
 	config = function()
 		require("chatgpt").setup({
 			api_key_cmd = "pass show my-keys/openai",
+			openai_params = {
+				model = "gpt-3.5-turbo",
+				frequency_penalty = 0,
+				presence_penalty = 0,
+				max_tokens = 1000,
+				temperature = 0,
+				top_p = 1,
+				n = 1,
+			},
 		})
 	end,
 	init = function()
