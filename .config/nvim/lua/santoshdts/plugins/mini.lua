2ce6b9b2caa600ff57605779d2a5be954fd36332
Santosh <ksantosh@intelops.dev>
2025-07-28 23:01:48 +0530

    update mini-surround and remove alpha

--- .config/nvim/lua/santoshdts/plugins/mini.lua
+++ .config/nvim/lua/santoshdts/plugins/mini.lua
@@ -1,48 +1,48 @@
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-		config = function()
-			-- Mini.ai: enhanced textobjects
-			require("mini.ai").setup({
-				n_lines = 500,
-				custom_textobjects = nil,
-			})
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+      -- Mini.ai: enhanced textobjects
+      require("mini.ai").setup({
+        n_lines = 500,
+        custom_textobjects = nil,
+      })
 
-			-- Mini.surround: powerful surrounding
-			require("mini.surround").setup({
-				mappings = {
-					add = "gsa", -- Add surrounding
-					delete = "gsd", -- Delete surrounding
-					replace = "gsr", -- Replace surrounding
-					find = "gsf", -- Find surrounding (to the right)
-				},
-			})
+      -- Mini.surround: powerful surrounding
+      require("mini.surround").setup(
+      -- mappings = {
+      -- 	add = "gsa", -- Add surrounding
+      -- 	delete = "gsd", -- Delete surrounding
+      -- 	replace = "gsr", -- Replace surrounding
+      -- 	find = "gsf", -- Find surrounding (to the right)
+      -- },
+      )
 
-			-- Mini.comment: commenting utilities
-			require("mini.comment").setup({
-				mappings = {
-					comment = "gc",
-					comment_line = "gcc",
-					comment_visual = "gc",
-				},
-			})
+      -- Mini.comment: commenting utilities
+      require("mini.comment").setup({
+        mappings = {
+          comment = "gc",
+          comment_line = "gcc",
+          comment_visual = "gc",
+        },
+      })
 
-			-- Mini.pairs: automatic pairing
-			require("mini.pairs").setup()
+      -- Mini.pairs: automatic pairing
+      require("mini.pairs").setup()
 
-			-- Mini.statusline: lightweight and configurable statusline
-			require("mini.statusline").setup({
-				set_vim_settings = true, -- override `laststatus`, `showmode`
-			})
+      -- Mini.statusline: lightweight and configurable statusline
+      require("mini.statusline").setup({
+        set_vim_settings = true, -- override `laststatus`, `showmode`
+      })
 
-			-- Optional: Disable default statusline for some filetypes
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = { "NvimTree", "alpha" },
-				callback = function()
-					vim.b.ministatusline_disable = true
-				end,
-			})
-		end,
-	},
+      -- Optional: Disable default statusline for some filetypes
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "NvimTree", "alpha" },
+        callback = function()
+          vim.b.ministatusline_disable = true
+        end,
+      })
+    end,
+  },
 }
