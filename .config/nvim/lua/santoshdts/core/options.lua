2da0b99db5323c4e9d41e282f78646d353577962
santoshkal <ksantosh@intelops.dev>
2025-07-29 16:45:06 +0530

    Set rounded winborder in global options

--- .config/nvim/lua/santoshdts/core/options.lua
+++ .config/nvim/lua/santoshdts/core/options.lua
@@ -7,7 +7,7 @@ local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
-
+opt.winborder = "rounded"
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
