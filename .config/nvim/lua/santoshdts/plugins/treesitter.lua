3b87299f6801761040e3cedd03ef4e85031d2548
santoshkal <ksantosh@intelops.dev>
2026-05-28 18:23:08 +0530

    Update treesitter to align with new 'main' branch config

--- .config/nvim/lua/santoshdts/plugins/treesitter.lua
+++ .config/nvim/lua/santoshdts/plugins/treesitter.lua
@@ -1,113 +1,147 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
-	branch = "master",
+	branch = "main",
 	build = ":TSUpdate",
 	dependencies = {
-		{ "nvim-treesitter/nvim-treesitter-textobjects" }, -- Syntax aware text-objects
 		{
-			"nvim-treesitter/nvim-treesitter-context", -- Show code context
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			branch = "main",
+		},
+		{
+			"nvim-treesitter/nvim-treesitter-context",
 			opts = { enable = true, mode = "topline", line_numbers = true },
 		},
 	},
 	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
+		local ts = require("nvim-treesitter")
 
-		-- configure treesitter
-		---@diagnostic disable-next-line: param-type-mismatch, missing-fields
-		treesitter.setup({
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"csv",
-				"dockerfile",
-				"gitignore",
-				"cue",
-				"go",
-				"gomod",
-				"gosum",
-				"gowork",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"proto",
-				"python",
-				"rego",
-				"sql",
-				"yaml",
-			},
-			sync_install = false, -- Install parsers asynchronously
-			auto_install = true, -- Auto-install missing parsers when entering buffer
-			-- enable syntax highlighting
-			highlight = {
-				enable = true,
-				disable = function(_, buf)
-					if vim.b[buf].bigfile then
-						return true
+		-- Bigfile detection (prevents TS on files > 500KB)
+		local function is_bigfile(buf)
+			if vim.b[buf].bigfile then
+				return true
+			end
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			return ok and stats and stats.size > 500 * 1024
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			callback = function()
+				local buf = vim.api.nvim_get_current_buf()
+				if is_bigfile(buf) then
+					vim.treesitter.highlighter.disable(buf, 0)
+				end
+			end,
+		})
+
+		-- Basic setup (parser install directory)
+		ts.setup({})
+
+		-- Install and manage parsers
+		local ensure_installed = {
+			"csv",
+			"dockerfile",
+			"gitignore",
+			"cue",
+			"go",
+			"gomod",
+			"gosum",
+			"gowork",
+			"javascript",
+			"json",
+			"lua",
+			"markdown",
+			"proto",
+			"python",
+			"rego",
+			"sql",
+			"yaml",
+		}
+		if vim.fn.executable("tree-sitter") == 1 then
+			vim.schedule(function()
+				pcall(ts.install, ensure_installed, { force = false })
+			end)
+		end
+
+		-- Tree-sitter based indentation
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "go", "python", "lua", "javascript", "json", "yaml", "proto", "sql", "markdown" },
+			callback = function()
+				if not is_bigfile(vim.api.nvim_get_current_buf()) then
+					vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+				end
+			end,
+		})
+
+		-- Incremental selection (manual since main branch removed define_modules)
+		local incremental_selection = {
+			init_selection = "<C-space>",
+			node_incremental = "<C-space>",
+			scope_incremental = "<leader>sc",
+			node_decremental = "<C-->",
+		}
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			callback = function()
+				local buf = vim.api.nvim_get_current_buf()
+				if is_bigfile(buf) then
+					return
+				end
+				local parser = vim.treesitter.get_parser(buf)
+				if not parser then
+					return
+				end
+				vim.keymap.set("n", incremental_selection.init_selection, function()
+					local root = parser:parse()[1]:root()
+					local cursor = vim.api.nvim_win_get_cursor(0)
+					local node = root:named_descendant_for_range(cursor[1] - 1, cursor[2], cursor[1] - 1, cursor[2])
+					if node then
+						vim.api.nvim_win_set_cursor(0, { node:start() + 1, 0 })
+						vim.cmd("normal! v")
+						vim.api.nvim_win_set_cursor(0, { node:end_() + 1, 0 })
 					end
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					return ok and stats and stats.size > 500 * 1024
-				end,
-			},
-			-- enable indentation
-			indent = {
-				enable = true,
-				disable = function(_, buf)
-					return vim.b[buf].bigfile == true
-				end,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = "<leader>sc",
-					node_decremental = "<C-->",
+				end, { buffer = buf, desc = "Init selection" })
+			end,
+		})
+
+		-- Textobjects configuration (main branch API)
+		require("nvim-treesitter-textobjects").setup({
+			select = {
+				lookahead = true,
+				include_surrounding_whitespace = true,
+				selection_modes = {
+					["@parameter.outer"] = "v",
+					["@function.outer"] = "V",
+					["@class.outer"] = "<c-v>",
 				},
 			},
-			textobjects = {
-				select = {
-					enable = true,
+		})
 
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
+		-- Textobject keymaps (main branch doesn't auto-create keymaps)
+		local textobject_select = require("nvim-treesitter-textobjects.select")
+		local textobject_keys = {
+			["af"] = { query = "@function.outer", desc = "Select around function" },
+			["if"] = { query = "@function.inner", desc = "Select inner function" },
+			["ac"] = { query = "@class.outer", desc = "Select around class" },
+			["ic"] = { query = "@class.inner", query_group = "textobjects", desc = "Select inner class" },
+			["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+		}
 
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						-- You can optionally set descriptions to the mappings (used in the desc parameter of
-						-- nvim_buf_set_keymap) which plugins like which-key display
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						-- You can also use captures from other query groups like `locals.scm`
-						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-					},
-					-- You can choose the select mode (default is charwise 'v')
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * method: eg 'v' or 'o'
-					-- and should return the mode ('v', 'V', or '<c-v>') or a table
-					-- mapping query_strings to modes.
-					selection_modes = {
-						["@parameter.outer"] = "v", -- charwise
-						["@function.outer"] = "V", -- linewise
-						["@class.outer"] = "<c-v>", -- blockwise
-					},
-					-- If you set this to `true` (default is `false`) then any textobject is
-					-- extended to include preceding or succeeding whitespace. Succeeding
-					-- whitespace has priority in order to act similarly to eg the built-in
-					-- `ap`.
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * selection_mode: eg 'v'
-					-- and should return true or false
-					include_surrounding_whitespace = true,
-				},
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			callback = function()
+				local buf = vim.api.nvim_get_current_buf()
+				if is_bigfile(buf) then
+					return
+				end
+				for lhs, spec in pairs(textobject_keys) do
+					vim.keymap.set({ "x", "o" }, lhs, function()
+						textobject_select.select_textobject(spec.query, spec.query_group)
+					end, { buffer = buf, desc = spec.desc })
+				end
+			end,
 		})
 	end,
 }
